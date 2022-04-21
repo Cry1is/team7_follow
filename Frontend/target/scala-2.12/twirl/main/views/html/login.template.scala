7ed329c09471565c08f8968a87f548c28f0d0d9e
myusernamesthisreal <myusernamesthis@gmail.com>
2022-04-20 22:19:07 -0500

    test

--- Frontend/target/scala-2.12/twirl/main/views/html/login.template.scala
+++ Frontend/target/scala-2.12/twirl/main/views/html/login.template.scala
@@ -107,8 +107,8 @@ Seq[Any](format.raw/*2.1*/("""<!DOCTYPE html>
 
               /*
                   -- GENERATED --
-                  DATE: 2022-02-22T11:59:16.041
-                  SOURCE: C:/Users/Zachary/Downloads/Lab-2-Ebean/Lab-2-Ebean/Frontend/app/views/login.scala.html
+                  DATE: 2022-04-20T22:05:25.574
+                  SOURCE: /home/myusernamesthis/cs4345/team7_follow/Frontend/app/views/login.scala.html
                   HASH: 35dca9e6949486fa9ac000f0cb94ff87b88db485
                   MATRIX: 948->1|1068->28|2656->1591|2671->1597|2722->1627|3477->2355|3513->2382|3552->2383|3609->2412|3712->2488|3749->2504|3806->2533|3902->2598|3955->2623|4183->2824|4198->2830|4243->2854
                   LINES: 28->1|33->2|62->31|62->31|62->31|78->47|78->47|78->47|79->48|80->49|80->49|81->50|83->52|84->53|87->56|87->56|87->56
